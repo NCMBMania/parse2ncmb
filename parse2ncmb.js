ffb4b11a8d8f73c6aa9ab9dadf30026386eea7d8
KOYAMA Tetsuji <koyama@hoge.org>
2016-09-28 11:50:27 +0900

    Add 2 phase process to import data.

--- parse2ncmb.js
+++ parse2ncmb.js
@@ -10,6 +10,7 @@
       , NCMB = require('ncmb')
       , program = require('commander')
       , Converter = require('./lib/converter')
+      , ObjMapper = require('./lib/objmapper')
       , throat = require('throat')
     ;
 
@@ -18,6 +19,8 @@
 	.usage('[options] <directory>')
 	.option('-c, --concurrency <number>', 'Set parallel concurrency',
 		parseInt)
+	.option('-p, --phase <number>', 'Specify phase number', parseInt)
+	.option('-s, --silent', 'Silent mode')
 	.parse(process.argv);
 
     if (!program.args.length) {
@@ -27,6 +30,14 @@
     var concurrency = 3; // default
     if (program.concurrency != undefined) {
 	concurrency = program.concurrency;
+    }
+    if (program.phase === undefined) {
+	console.error('Phase number required');
+	process.exit(1);
+    }
+    if (program.phase != 1 && program.phase != 2) {
+	console.error('Invalid phase number');
+	process.exit(1);
     }
 
     let targetDir = program.args[0];
@@ -50,12 +61,17 @@
 	let file = path.replace(/.*\//, '');
 
 	if (file.substr(0, joinPrefix.length) === joinPrefix) {
+	    // join type
 	    type = 'join';
-	    name = file;
+	    let match = file.match(/^_Join:(.*)\.json$/);
+	    name = match[1];
 	} else if (typemap[file] !== undefined) {
+	    // preset types
 	    type = typemap[file];
-	    name = '';
+	    let match = file.match(/(.*)\.json$/);
+	    name = match[1];
 	} else {
+	    // other
 	    type = 'object';
 	    let match = file.match(/(.*)\.json$/);
 	    if (match === null) {
@@ -72,28 +88,16 @@
 	};
     }
 
-    var fileInfos = [];
+    var objFiles = [], relFiles = [];
     files.forEach(function(path) {
 	let info = getPathInfo(path);
-	if (info.type !== null) {
-	    fileInfos.push(info);
+	if (info.type === null) {
+	    return;
 	}
-    });
-
-    // sort for 'join' type will be end of array
-    fileInfos.sort(function(a, b) {
-	if (a.type == 'join') {
-	    if (b.type == 'join') {
-		return a.path - b.path;
-	    } else {
-		return 1;
-	    }
+	if (info.type == 'join') {
+	    relFiles.push(info);
 	} else {
-	    if (b.type == 'join') {
-		return -1;
-	    } else {
-		return a.path - b.path;
-	    }
+	    objFiles.push(info);
 	}
     });
 
@@ -101,11 +105,23 @@
     let app_key = Configfile.config.app_key;
     let client_key = Configfile.config.client_key;
     var ncmb = new NCMB(app_key, client_key);
+    var objMapper = new ObjMapper;
 
     var Parallel = throat(Promise)(concurrency);
 
-    fileInfos.forEach(function(info) {
-	var converter = new Converter(ncmb, info.type, info.name);
+    let targetFiles = null;
+    if (program.phase == 1) {
+	// Phase 1: store objects
+	targetFiles = objFiles;
+	objMapper.reset();
+    } else {
+	// Phase 2: store relations and others
+	targetFiles = relFiles;
+	objMapper.ensureIndex();
+    }
+
+    targetFiles.forEach(function(info) {
+	var converter = new Converter(ncmb, info.type, info.name, objMapper);
 
 	fs.createReadStream(info.path)
 	    .pipe(JSONStream.parse('results.*'))
@@ -114,14 +130,11 @@
 		    return converter.convert(data)
 		})
 		.then(function(results) {
-		    // something on success?
 		})
 		.catch(function(err) {
-		    console.log(err);
-		})
-		;
+		    console.error(err);
+		});
 	    });
     });
-    return;
 
 })((this || 0).self || global);
